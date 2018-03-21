d7dd24d262b95c8e28907250f3809bb093e2aa65
Jeff Yeon <jeff@wbrandstudio.com>
2018-03-21 15:27:07 -0700

    added geolocation

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -32,6 +32,46 @@
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,800" rel="stylesheet">
+
+        <script>
+        var x = document.getElementById("demo");
+
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition, showError);
+            } else {
+                x.innerHTML = "Geolocation is not supported by this browser.";
+            }
+        }
+
+        function showPosition(position) {
+            var latlon = position.coords.latitude + "," + position.coords.longitude;
+            var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
+                +latlon+"&zoom=14&size=400x300&key=AIzaSyCztjrnqMbLtW2T851EMcrSLkY6DcIINPg";
+            document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+        }
+        //To use this code on your website, get a free API key from Google.
+        //Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
+
+        function showError(error) {
+            switch(error.code) {
+                case error.PERMISSION_DENIED:
+                    x.innerHTML = "User denied the request for Geolocation."
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    x.innerHTML = "Location information is unavailable."
+                    break;
+                case error.TIMEOUT:
+                    x.innerHTML = "The request to get user location timed out."
+                    break;
+                case error.UNKNOWN_ERROR:
+                    x.innerHTML = "An unknown error occurred."
+                    break;
+            }
+        }
+        </script>
+
+
         <meta name="csrf-token" content="{{ csrf_token() }}">
         @if(app()->isLocal())
             <link rel="stylesheet" href="{{ asset('css/app.css') }}">
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -12,11 +12,17 @@
             </div>
             <div class="col-sm-2 sidenav right">
                 <div class="location">
+                    <p>Select Your State: </p>
                     <select name="country" id="country">
                         @foreach(CountryState::getStates('US') as $state)
                             <option value="{{$state}}">{{ $state }}</option>
                         @endforeach
                     </select>
+
+                    <p id="demo">Or Click The Button To Select Your Current Location</p>
+
+                    <button onclick="getLocation()">Select Current Location</button>
+                    <div id="mapholder"></div>
                 </div>
             </div>
 
