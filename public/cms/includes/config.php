55fc1cb8fecf90f5591714a8e4169f98453b8b0a
Jeff Yeon <jeff@wbrandstudio.com>
2018-03-22 09:37:36 -0700

    changed db

--- public/cms/includes/config.php
+++ public/cms/includes/config.php
@@ -1,8 +1,8 @@
 <?php
 $config['db']['host'] = 'localhost';
-$config['db']['name'] = 'test';
-$config['db']['user'] = 'root';
-$config['db']['pass'] = '';
+$config['db']['name'] = 'poorhub';
+$config['db']['user'] = 'pooradmin';
+$config['db']['pass'] = '1Lyakl4e!';
 $config['db']['pre'] = 'ad_';
 
 $config['version'] = '6';
